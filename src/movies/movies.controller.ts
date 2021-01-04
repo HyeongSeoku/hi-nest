7b9ae1253fd5f40c8f3d83a88e79bf01631d7114
gudtjr3437 <gudtjr3437@naver.com>
2021-01-04 22:58:03 +0900

    Update movies.controller.ts

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,43 +1,39 @@
-import { Controller, Delete, Get, Param, Post, Patch, Body, Query } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Patch, Body, Query, NotFoundException } from '@nestjs/common';
+import { CreateMovieDto} from './dto/create-movie.dto';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('/movies')   // ('movies') : Entry Point를 컨트롤함
 export class MoviesController {
 
+    constructor(private readonly moviesService: MoviesService){}
+
     @Get()
-    getAll(){
-        return `This will return all movies`;
-    }
-    
-    // @Get('/:id') 보다 아래에 있으면 "search"를 id로 판단함
-    @Get('search')
-    search(@Query("year") seachingYear:string){
-        return `We are searching for a movie made after :`;
+    getAll():Movie[]{
+        return this.moviesService.getAll();
     }
 
     //('/:id')와 @Param('id')는 동일해야함 
     //movieId:string 은 위와 동일하지 않아도됨
     @Get('/:id')
-    getOne(@Param('id') movieId:string){
-        return `This will return one movie with the id: ${movieId}`;
+    getOne(@Param('id') movieId:string) :Movie{
+        return this.moviesService.getOne(movieId);
     }
 
     @Post()
-    create(@Body() movieData){
-        return movieData;
+    create(@Body() movieData: CreateMovieDto){
+        return this.moviesService.create(movieData);
     }
 
     @Delete("/:id")
     remove(@Param('id') movieId:string){
-        return `This will delete a movie withe the id: ${movieId}`;
+        return this.moviesService.deleteOne(movieId);
     }
 
     //patch = 일부분만 업데이트 할때 , Put 전체 업데이트
     @Patch('/:id')
     patch(@Param('id') movieId:string, @Body() updateData){
-        return {
-            updatedMovie: movieId,
-            ...updateData,
-        }
+        return this.moviesService.update(movieId,updateData);
     }
     
 }
