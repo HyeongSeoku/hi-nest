afa8d39d3e820648b11b8de227241cfd365e733f
gudtjr3437 <gudtjr3437@naver.com>
2021-01-04 22:57:08 +0900

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
 
 
 //데코레이터 = > 클래스에 함수기능을 추가할수 있음
 @Module({
   imports: [],
   controllers: [MoviesController],
-  providers: [],
+  providers: [MoviesService],
 })
 export class AppModule {}
