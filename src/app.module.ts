1eb8e2755b3c4a2b28648f0d42588075191f30d9
gudtjr3437 <gudtjr3437@naver.com>
2021-01-05 03:20:34 +0900

    Update app.module.ts
    MoviesController 와 MoviesService 를 MoviesModule에 포함시킴

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
 import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
+
 
 
 //데코레이터 = > 클래스에 함수기능을 추가할수 있음
 @Module({
-  imports: [],
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
