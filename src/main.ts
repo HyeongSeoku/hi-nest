be97b8df49c14f78adb96a82ba97d80c66eb6613
gudtjr3437 <gudtjr3437@naver.com>
2021-01-04 23:00:36 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,   //이상한걸 보내면 Request 자체를 막아버림
+    transform: true,    //유저들이 보낸것을 우리가 원하는 실제 타입으로 변환
   }));
   await app.listen(3000);
 }
