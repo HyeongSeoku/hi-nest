4a72e775683eb69ee4143626a9b6d1d3addb1511
gudtjr3437 <gudtjr3437@naver.com>
2021-01-04 22:57:48 +0900

    Update main.ts
    유효성 검사
    whitelist
    forbidNonWhitelisted

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,   //이상한걸 보내면 Request 자체를 막아버림
+  }));
   await app.listen(3000);
 }
 bootstrap();
