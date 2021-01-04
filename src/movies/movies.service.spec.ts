050ca5970a90f9ccdde6fbb3d3b696ea4b98deb3
gudtjr3437 <gudtjr3437@naver.com>
2021-01-05 03:21:34 +0900

    테스트 파일

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,9 +1,11 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
   let service: MoviesService;
 
+  //테스트 하기전 실행되는부분
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [MoviesService],
@@ -12,7 +14,37 @@ describe('MoviesService', () => {
     service = module.get<MoviesService>(MoviesService);
   });
 
+  //테스트 되는 부분
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe("getAll",()=>{
+    it("should return an array",()=>{
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array); 
+    })
+  })
+
+  describe("getOne",()=>{
+    it("should return a movie",()=>{
+
+    service.create({
+      title : "Test Movie",
+      genres: ['test'],
+      year: 2000,
+    });
+    const movie= service.getOne(1);
+    expect(movie).toBeDefined();
+    expect(movie.id).toEqual(1);
+    });
+    it("should throw 404 error",()=>{
+      try{
+        service.getOne(999);
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    })
+  });
+
 });
