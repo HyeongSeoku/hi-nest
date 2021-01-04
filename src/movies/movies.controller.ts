e36406fe87d6e4447aa26d3d392893d78c65e3a2
gudtjr3437 <gudtjr3437@naver.com>
2021-01-04 16:21:51 +0900

    Update movies.controller.ts

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
-@Controller('movies')   // ('movies') : Entry Point를 컨트롤함
+@Controller('/movies')   // ('movies') : Entry Point를 컨트롤함
 export class MoviesController {
 
     @Get()
     getAll(){
-        return "This will return all movies";
+        return `This will return all movies`;
+    }
+
+    //('/:id')와 @Param('id')는 동일해야함 
+    //id:string 은 위와 동일하지 않아도됨
+    @Get('/:id')
+    getOne(@Param('id') id:string){
+        return `This will return one movie with the id: ${id}`;
     }
 }
