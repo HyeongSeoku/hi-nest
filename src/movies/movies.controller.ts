213aff18550f5a6148e26377e2203a9eddfb3e4b
gudtjr3437 <gudtjr3437@naver.com>
2021-01-04 16:29:48 +0900

    Update movies.controller.ts
    Post,Delete,Patch 기능

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Patch } from '@nestjs/common';
 
 @Controller('/movies')   // ('movies') : Entry Point를 컨트롤함
 export class MoviesController {
@@ -9,9 +9,27 @@ export class MoviesController {
     }
 
     //('/:id')와 @Param('id')는 동일해야함 
-    //id:string 은 위와 동일하지 않아도됨
+    //movieId:string 은 위와 동일하지 않아도됨
     @Get('/:id')
-    getOne(@Param('id') id:string){
-        return `This will return one movie with the id: ${id}`;
+    getOne(@Param('id') movieId:string){
+        return `This will return one movie with the id: ${movieId}`;
     }
+
+    @Post()
+    create(){
+        return "This will create a movie";
+    }
+
+    @Delete("/:id")
+    remove(@Param('id') movieId:string){
+        return `This will delete a movie withe the id: ${movieId}`;
+    }
+
+    //patch = 일부분만 업데이트 할때 , Put 전체 업데이트
+    @Patch('/:id')
+    patch(@Param('id') movieId:string){
+        return `This will update a movie withe the id: ${movieId}`;
+    }
+
+    
 }
